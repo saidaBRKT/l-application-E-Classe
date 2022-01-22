c6ef6901e0cf408a94ca9c6e58fef552c6e31218
SAIDAbrkt <saidabarakat06@gmail.com>
2022-01-22 18:07:20 +0100

    update

--- back-end/pages_php/home.php
+++ back-end/pages_php/home.php
@@ -20,62 +20,7 @@
 <body>
   <main class="container-fluid">
     <div class="row flex-nowrap">
-      <div class="cont col-auto col-lg-2 px-0 sticky-top h-100">
-        <div class="d-flex flex-column align-items-center  px-3 pt-2 text-white h-100 min-vh-100">
-          <a href="/" class=" title d-flex align-items-center pb-3 text-dark text-decoration-none">
-            <div class="fs-4 d-none d-sm-inline">
-              <h1>E-classe</h1>
-            </div>
-          </a>
-          <div class="profile">
-            <img src="../images/people-peoples-homeless-male.jpg"
-              class="rounded-circle d-none d-sm-flex d-flex flex-column justify-content-center align-items-center"
-              alt="profile">
-            <div class="admin d-flex flex-column justify-content-center align-items-center mb-3">
-              <span class="text-dark d-none d-sm-flex">Name Admin</span>
-              <span class="text-primary d-none d-sm-flex">Admin</span>
-            </div>
-          </div>
-          <ul class="nav nav-pills flex-column mb-0 mt-0  align-items-md-center" id="menu">
-            <li class="nav-item li_home ">
-              <a href="#" class="nav-link align-middle px-0 text-dark">
-                <i class="fal fa-home-lg-alt"></i> <span class="ms-1 d-none d-sm-inline">Home</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="#" class="nav-link align-middle px-0 text-dark">
-                <i class="far fa-bookmark"></i> <span class="ms-1 d-none d-sm-inline">Cours</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="students.php" class="nav-link align-middle px-0 text-dark">
-                <i class="fal fa-graduation-cap"></i> <span class="ms-1 d-none d-sm-inline">Students</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="payment.php" class="nav-link align-middle px-0 text-dark">
-                <i class="fal fa-usd-square"></i> <span class="ms-1 d-none d-sm-inline">Payment</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="#" class="nav-link align-middle px-0 text-dark">
-                <i class="fal fa-file-chart-line"></i> <span class="ms-1 d-none d-sm-inline">Report</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="#" class="nav-link align-middle px-0 text-dark">
-                <i class="fal fa-sliders-v-square"></i> <span class="ms-1 d-none d-sm-inline">Setting</span>
-              </a>
-            </li>
-            <li class="nav-item mt-3 mt-0">
-              <a href="../index.php" class="nav-link align-middle px-0 text-dark">
-                <span class="ms-1 d-none d-sm-inline">logout</span> <i class="fal fa-sign-out-alt"></i>
-              </a>
-            </li>
-
-          </ul>
-        </div>
-      </div>
+    <?php include 'sidebar.php' ?>
       <div class="col d-flex flex-column">
 
        <?php include 'header.php' ?>
